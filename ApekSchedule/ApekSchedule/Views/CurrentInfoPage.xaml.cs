5308dd17bf70e0ad26d0b541b1cbb997beb191df
Владимир <MoonFox923@gmail.com>
2024-03-29 00:03:35 +0700

    make pre-version of current lesson info

--- ApekSchedule/ApekSchedule/Views/CurrentInfoPage.xaml.cs
+++ ApekSchedule/ApekSchedule/Views/CurrentInfoPage.xaml.cs
@@ -6,103 +6,175 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ApekSchedule.ViewModels;
+using ApekSchedule.Models;
 
 namespace ApekSchedule.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CurrentInfoPage : ContentPage
 	{
-		//private readonly AsiecParser _asiecParser;
-		//private Day _currentDay;
-		//private bool is_studyTime;
+		private static Day _currentDay;
+
+		private bool _isDebug;
+
+		public static Day CurrentDay
+		{
+			set => _currentDay = value;
+		}
 
 		public CurrentInfoPage()
 		{
 			InitializeComponent();
-
-			//_asiecParser = new AsiecParser();
-			//is_studyTime = true;
+			_isDebug = true;
 		}
 
-		//protected async override void OnAppearing()
-		//{
-		//	//Schedule curDaySchedule = await _asiecParser.GetSchedule("9ИСиП231", DateTime.Now, DateTime.Now);
-		//	//_currentDay = curDaySchedule.Days.Count != 0 ? curDaySchedule.Days[0] : new Day() { Date = DateTime.Now };
+		protected async override void OnAppearing()
+		{
+			Schedule curDaySchedule = await App.AsiecParser.GetSchedule("9ИСиП231", DateTime.Now, DateTime.Now);
+			_currentDay = curDaySchedule.Days.Count != 0 ? curDaySchedule.Days[0] : new Day() { Date = DateTime.Now };
 
-		//	//await SetCurrentInfo();
-		//}
+			await SetCurrentInfo();
+		}
 
-		//private async Task SetCurrentInfo()
-		//{
-		//	if (_currentDay.Date.Date != DateTime.Now.Date)
-		//	{
-		//		await DisplayAlert("Ошибка", "Текущая дата не совпадает с полученной", "ОК");
-		//		return;
-		//	}
+		private async Task SetCurrentInfo()
+		{
+			if (_currentDay.Date.Date != DateTime.Now.Date)
+			{
+				await DisplayAlert("Ошибка", "Текущая дата не совпадает с полученной", "ОК");
+				return;
+			}
 
-		//	if (_currentDay.Lessons == null)
-		//	{
-		//		CurrentInfoLabel.Text = "Сегодня выходной";
-		//		return;
-		//	}
+			HideCurrentLessonInfo();
+			EndTimerFrame.IsVisible = true;
 
-		//	TimeSpan prevEnd = TimeSpan.Zero;
-		//	is_studyTime = true;
+			if (_currentDay.Lessons == null)
+			{
+				SetOnlyCurrentLessonName("Сегодня выходной");
+				return;
+			}
 
-		//	foreach (Lesson lesson in _currentDay.Lessons)
-		//	{
+			TimeSpan prevEnd = TimeSpan.Zero;
+			TimeSpan firstTime = _currentDay.Lessons[0].StartTime;
 
-		//		TimeSpan start = lesson.StartTime;
-		//		TimeSpan end = lesson.EndTime;
-		//		TimeSpan current = DateTime.Now.TimeOfDay;
+			TimeSpan currentTime = _isDebug ? DatePicker.Time : DateTime.Now.TimeOfDay;
 
-		//		bool is_lessonTime = current >= start && current <= end;
-		//		bool is_restTime = (current >= prevEnd && current <= start) && prevEnd != TimeSpan.Zero;
+			foreach (Lesson lesson in _currentDay.Lessons)
+			{
 
-		//		prevEnd = lesson.EndTime;
+				TimeSpan start = lesson.StartTime;
+				TimeSpan end = lesson.EndTime;
 
-		//		if (is_lessonTime || is_restTime)
-		//		{
-		//			TimeSpan endTimer = is_lessonTime ? end : start;
-		//			string infoMessage = is_lessonTime ? lesson.ToString() : "Перемена";
+				bool is_lessonTime = currentTime >= start && currentTime <= end;
+				bool is_restTime = (currentTime >= prevEnd && currentTime <= start) && prevEnd != TimeSpan.Zero;
 
-		//			CurrentInfoLabel.Text = infoMessage;
-		//			StartTimer(endTimer);
-		//			return;
-		//		}
-		//	}
+				prevEnd = lesson.EndTime;
 
-		//	is_studyTime = false;
-		//	CurrentInfoFrame.IsVisible = false;
-		//	CurrentInfoLabel.Text = "Сейчас не учебное время";
-		//}
+				if (is_lessonTime || is_restTime)
+				{
+					TimeSpan endTimer = is_lessonTime ? end : start;
+					
+					if (!is_lessonTime)
+					{
+						SetOnlyCurrentLessonName("Перемена");
+					}
+					else
+					{
+						SetCurrentLessonInfo(lesson);
+					}
+
+					
+					StartTimer(endTimer);
+					return;
+				}
+			}
+
+			if (currentTime < firstTime)
+			{
+				SetOnlyCurrentLessonName("Пары ещё не начались");
+			}
+
+			if (prevEnd < currentTime)
+			{
+				SetOnlyCurrentLessonName("Пары закончились");
+			}
+
+			EndTimerFrame.IsVisible = false;
+		}
+
+		private void SetCurrentLessonInfo(Lesson currentLesson)
+		{
+			CurrentLessonNameLabel.Text = $"Пара: {currentLesson.Name}";
+			CurrentLessonTeacherLabel.Text = $"Преподаватель: {currentLesson.Teacher}";
+			CurrentLessonClassroomLabel.Text = $"Аудитория: {currentLesson.Classroom}";
+			CurrentLessonTimeLabel.Text = $"Время: {currentLesson.StartTime:hh\\:mm} - {currentLesson.EndTime:hh\\:mm}";
+
+			CurrentLessonNameLabel.IsVisible = true;
+			CurrentLessonTeacherLabel.IsVisible = true;
+			CurrentLessonClassroomLabel.IsVisible = true;
+			CurrentLessonTimeLabel.IsVisible = true;
+		}
+
+		private void SetOnlyCurrentLessonName(string name)
+		{
+			HideCurrentLessonInfo();
+			CurrentLessonNameLabel.IsVisible = true;
+			CurrentLessonNameLabel.Text = name;
+		}
+
+		private void HideCurrentLessonInfo()
+		{
+			CurrentLessonNameLabel.IsVisible = false;
+			CurrentLessonTeacherLabel.IsVisible = false;
+			CurrentLessonClassroomLabel.IsVisible = false;
+			CurrentLessonTimeLabel.IsVisible = false;
+		}
 
 
+		private void StartTimer(TimeSpan end)
+		{
+			Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+			{
+				TimeSpan currentTime = _isDebug ? DatePicker.Time : DateTime.Now.TimeOfDay;
+				TimeSpan duration = end - currentTime;
+				EndTimerLabel.Text = duration.ToString(@"hh\:mm\:ss");
 
+				if (duration.TotalSeconds <= 0)
+				{
+					Task.Run(async () =>
+					{
+						await SetCurrentInfo();
+					});
+					return false;
+				}
 
-		//private void StartTimer(TimeSpan end)
-		//{
-		//	if (!is_studyTime)
-		//	{
-		//		return;
-		//	}
+				return true;
+			});
 
-		//	Device.StartTimer(TimeSpan.FromSeconds(1), () =>
-		//	{
-		//		TimeSpan duration = end - DateTime.Now.TimeOfDay;
-		//		EndTimerLabel.Text = duration.ToString(@"hh\:mm\:ss");
+			
+		}
 
-		//		if (duration.TotalSeconds <= 0)
-		//		{
-		//			Task.Run(async () =>
-		//			{
-		//				await SetCurrentInfo();
-		//			});
-		//			return false;
-		//		}
+		private async void SetTimeButton_Clicked(object sender, EventArgs e)
+		{
+			await SetCurrentInfo();
+		}
 
-		//		return true;
-		//	});
-		//}
+		private async void DebugButton_Clicked(object sender, EventArgs e)
+		{
+			if (_isDebug)
+			{
+				_isDebug = false;
+				DatePicker.IsVisible = false;
+				SetTimeButton.IsVisible = false;
+			}
+			else
+			{
+				_isDebug = true;
+				DatePicker.IsVisible = true;
+				SetTimeButton.IsVisible = true;
+			}
+
+			await SetCurrentInfo();
+		}
 	}
 }
